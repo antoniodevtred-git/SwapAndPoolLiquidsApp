7bd5e2e8ef730a2c050f245aa4940be698faf598
antonio.devtred@gmail.com <antonio.devtred@gmail.com>
2025-12-23 16:15:15 +0100

    identations deleted

--- BackEnd/SwapLiquidityAppBackEnd/src/mocks/MockRouter.sol
+++ BackEnd/SwapLiquidityAppBackEnd/src/mocks/MockRouter.sol
@@ -19,18 +19,11 @@ contract MockRouter is IV2Router02 {
         tokenB = tokenB_;
     }
 
-    function addLiquidity(
-        address, address, uint256, uint256, uint256, uint256, address, uint256
-    ) external pure override returns (uint256, uint256, uint256) {
+    function addLiquidity(address, address, uint256, uint256, uint256, uint256, address, uint256) external pure override returns (uint256, uint256, uint256) {
         return (100, 100, 10); // valores mock
     }
 
-    function swapExactTokensForTokens(
-        uint256 amountIn_,
-        uint256 amountOutMin_,
-        address[] calldata path_,
-        address to_,
-        uint256
+    function swapExactTokensForTokens(uint256 amountIn_, uint256 amountOutMin_, address[] calldata path_, address to_, uint256
     ) external override returns (uint256[] memory) {
         IERC20(path_[0]).transferFrom(msg.sender, address(this), amountIn_);
         IERC20(path_[1]).transfer(to_, amountOutMin_);
@@ -41,15 +34,7 @@ contract MockRouter is IV2Router02 {
         return result;
     }
 
-    function removeLiquidity(
-        address, 
-        address, 
-        uint256 liquidity, 
-        uint256, 
-        uint256, 
-        address to, 
-        uint256
-    ) external override returns (uint256 amountA, uint256 amountB) {
+    function removeLiquidity(address,  address,  uint256 liquidity,  uint256,  uint256,  address to,  uint256) external override returns (uint256 amountA, uint256 amountB) {
         // Suponemos una proporción fija para simular resultados
         amountA = liquidity / 2;
         amountB = liquidity / 2;
